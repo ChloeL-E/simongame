4531d278f497ed09499bdbe2a2fd5710ec9ec00a
ChloeL-E <chloe_c123@hotmail.co.uk>
2024-01-07 21:20:21 +0000

    added new tests and functions

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,5 +1,9 @@
 
-const { game, newGame, showScore } = require("../game");
+
+
+const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require("../game");
+
+jest.spyOn(window, "alert").mockImplementation(() => { });
 
 beforeAll(() => {
     let fs = require("fs");
@@ -9,7 +13,7 @@ beforeAll(() => {
     document.close();
 })
 
-describe("game object contians the correct keys", () => {
+describe("game object contains the correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
     });
@@ -25,6 +29,18 @@ describe("game object contians the correct keys", () => {
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    })
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    })
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+    }) 
+    test("turnInProgress key is set to false", () => {
+        expect("turnInProgress" in game).toBe(true);
+    })
 });
 
 describe("newGame works correctly", () => {
@@ -47,4 +63,62 @@ describe("newGame works correctly", () => {
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
+});
+
+describe('gameplay works correctly', () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    })
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
+    });
+    test("showTurn should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
+    });
+    test("turnNumber should be reset to 0 ", () => {
+        game.turnNumber = 42;
+    });
+    test("expect data-listener to be true", () => {
+        newGame();
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }
+    });
+    test("should increment the score if the turn is correct", () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    });
+    test("should call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("wrong move!");
+    });
+    test("should toggle turnInProgress to be true", () => {
+       showTurns();
+       expect(game.turnInProgress).toBe(true);
+    })
+    test("clicking during computer sequence should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();//there should be no id in there if clicks are disabled. the contents of last button shouldnt change after clicks are disabled
+        expect(game.lastButton).toEqual("");
+    })
 });
